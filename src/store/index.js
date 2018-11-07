ca2091e82e38cc76528a29baa6d729d142d091b9
VovaSakai <vovas2345@gmail.com>
2018-11-07 18:29:38 +1000

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import localStorage, { loadState } from '../middleware/local-store';
+
+import reducer from '../reducers';
+
+const initialState =  loadState('todos');
+const store = createStore(reducer, initialState, applyMiddleware(thunk, promise, localStorage('todos'), logger));
+
+export default store;
